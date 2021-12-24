905f57921dd446ee401f3a0ef47648fe104d30e3
Mason Fibkins <mason.fibkins@gmail.com>
2021-12-24 14:22:50 -0700

    fixed files for new repo

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync Sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
