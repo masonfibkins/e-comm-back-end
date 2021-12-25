e7555be5776a79305248c3ad20cb30702e067874
Mason Fibkins <mason.fibkins@gmail.com>
2021-12-24 23:35:41 -0700

    fixed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require("./config/connection");
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
